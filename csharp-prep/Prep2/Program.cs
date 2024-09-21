4cffedcd8525e7f18ae51f2ba56871940a52175b
annakelsey1 <anna.kelsey@gmail.com>
2024-09-21 09:31:52 -0600

    Program 2! C# kina sucks!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Input your grade percentage:");
+        string getGrade = Console.ReadLine();
+        int grade = int.Parse(getGrade);
+        string inputGrade;
+
+        if (grade >= 90)
+        {
+            inputGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            inputGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            inputGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            inputGrade = "D";
+        }
+        else
+        {
+            inputGrade = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Your grade is {inputGrade}. Congratulation, you passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {inputGrade}. You didn't pass this time, but I'm sure you will next time!");
+        }
     }
 }
