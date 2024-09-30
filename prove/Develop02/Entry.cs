79791cd486ac49057f0f1a44336e797d586d8038
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-09-30 17:11:36 -0600

    Update Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,3 +2,4 @@ using System;
 //Write a new entry - Show the user a random prompt (from a list that you create), 
 //and save their response, the prompt, and the date as an Entry.
 
+//Display the journal - Iterate through all entries in the journal and display them to the screen.
