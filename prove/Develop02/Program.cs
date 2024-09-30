bef8e2dd630d61e6783e104b7d51c7997d36cb7f
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-09-30 17:13:26 -0600

    Update Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,13 @@
 using System;
 
+//Save the journal to a file - Prompt the user for a filename and then save 
+//the current journal (the complete list of entries) to that file location.
+
+//Load the journal from a file - Prompt the user for a filename and then load 
+//the journal (a complete list of entries) from that file. This should replace any entries currently stored the journal.
+
+//Provide a menu that allows the user choose these options
+
 class Program
 {
     static void Main(string[] args)
