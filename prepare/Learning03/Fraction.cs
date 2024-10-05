a2372b0114e2b0c25134a51403b75ccba4170845
anna <anna.kelsey@gmail.com>
2024-10-04 22:02:13 -0600

    done!

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,1 +1,52 @@
+using System;
 
+public class Fraction
+    {
+        private int _top;
+        private int _bottom;
+
+        public Fraction()
+        {
+            _top = 1;
+            _bottom = 1;
+        }
+
+        public Fraction(int top)
+        {
+            _top = top;
+            _bottom = 1;
+        }
+
+        public Fraction(int top, int bottom)
+        {
+            _top = top;
+            _bottom = bottom;
+        }
+
+        public override string ToString()
+        {
+            return $"{_top}/{_bottom}";
+        }
+
+        public int Top
+        {
+            get {return _top;}
+            set {_top = value;}
+        }
+
+        public int Bottom
+        {
+            get {return _bottom;}
+            set {_bottom = value;}
+        }
+
+        public string GetFractionString()
+        {
+            return $"{_top}/{_bottom}";
+        }
+
+        public double GetDecimalValue()
+        {
+            return (double)_top / _bottom;
+        }
+    }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+    //     Console.WriteLine("Hello Learning03 World!");
+
+        // Fraction fraction1 = new Fraction();      
+        // Fraction fraction2 = new Fraction(5);     
+        // Fraction fraction3 = new Fraction(3, 4); 
+
+        // Console.WriteLine(fraction1);
+        // Console.WriteLine(fraction2);
+        // Console.WriteLine(fraction3);   
+
+        Fraction fraction = new Fraction(5, 2);
+        // Console.WriteLine($"Top: {fraction.Top}");
+        // Console.WriteLine($"Bottom: {fraction.Bottom}");
+
+        // fraction.Top = 3;
+        // fraction.Bottom = 7;
+        // Console.WriteLine(fraction);
+
+        string fractionString = fraction.GetFractionString();
+        double decimalValue = fraction.GetDecimalValue();
+
+        Console.WriteLine($"Fraction: {fractionString}");
+        Console.WriteLine($"Decimal: {decimalValue}");
     }
+
 }
