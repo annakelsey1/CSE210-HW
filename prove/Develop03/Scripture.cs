6f18fa9921cdb7bb87abe45db617f73ace792fb5
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-10-06 19:23:11 -0600

    Create Scripture.cs

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,6 @@
+// Scripture: Keeps track of both the reference and the text of the scripture. 
+// Can hide words and get the rendered display of the text.
+
+// Reference: Keeps track of the book, chapter, and verse information.
+
+// Word: Keeps track of a single word and whether it is shown or hidden.
