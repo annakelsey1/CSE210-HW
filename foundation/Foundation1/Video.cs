e206fcfd9d05f7acdeab8d294b03b20a75fad1cf
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-10-10 21:14:39 -0600

    Create Video.cs

---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,4 @@
+// “Video” Class:
+// _title : string  |  keeps track of the title of the video the add plays on
+//             _author : string  |  keeps track of the name of the youtuber
+//             _length : int  |  keeps track of how long the video is
