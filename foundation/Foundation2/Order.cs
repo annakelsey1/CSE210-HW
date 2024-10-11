24a55226963687e664ad6b37abe825891ac84ada
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-10-10 21:15:56 -0600

    Create Order.cs

---
+++ foundation/Foundation2/Order.cs
@@ -0,0 +1,6 @@
+// “Order” Class:
+// 	List<string> order = new List<string>();  |  Creates a new list named “order”
+// 	For code that goes through the list of costs and adds them
+// 	If then statement that determines shipping and adds to cost
+// 	Packing Label  |  Puts the name and a for loop to list all products in order
+// 	Shipping Label(name, address)  |  Creates a shipping label
