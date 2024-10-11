457e3ea4d033cb40ec6b76097fa7d0ccb50f7a54
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-10-10 21:16:27 -0600

    Create Product.cs

---
+++ foundation/Foundation2/Product.cs
@@ -0,0 +1,7 @@
+// “Product” Class:
+// String ProductID  |  creates the product Id
+// String PName  |  Product name
+// Decimal Price  |  price of item
+// Int Quantity  |  How many of each item
+// Product(ID, PName, Price, Quantity)  |  Collecting everything into one place
+// GetTotal()  |  Gets total cost
