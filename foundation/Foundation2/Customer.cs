9c3feac694317f3399c27f4b3f52a0aab9ac2e23
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-10-10 21:16:53 -0600

    Create Customer.cs

---
+++ foundation/Foundation2/Customer.cs
@@ -0,0 +1,5 @@
+// “Customer” Class:
+//  	String CustomerName  |  Gets customer name
+// 	String Address  |  Gets address
+// 	Customer(CustomerName, Address)  |  stores the name and address together
+// 	Bool USA  |  Keeps track of if the customer is in the US or not
