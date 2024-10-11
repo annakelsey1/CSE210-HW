6ffe345e8b2837a670d265035bd6897448ac1b70
annakelsey1 <167836058+annakelsey1@users.noreply.github.com>
2024-10-10 21:17:18 -0600

    Create Address.cs

---
+++ foundation/Foundation2/Address.cs
@@ -0,0 +1,4 @@
+// “Address” Class:
+//  A string for Street, City, State and Country  |  Sets up variables for the address
+// Address(Street, City, State, Country)  |  Method that stores the addresses together
+// US or not  |  Method that returns if they’re in the usa, will be called in order class
